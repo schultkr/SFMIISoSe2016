4fe4f7e9e147c2be75913af34b4183d7a0a24f1c
schultkr <schult.christoph@googlemail.com>
2016-05-09 11:37:17 +0200

    update

--- SFESimADF/SFESimADFV2.R
+++ SFESimADF/SFESimADFV2.R
@@ -16,13 +16,13 @@ lapply(libraries, library, quietly = TRUE, character.only = TRUE)
 # define number of simulations for ADF test
 iSimulations = 1000
 # define sample size
-iSample      = 1000
+iSample      = 100
 # define beta parmaters
 beta = c(-0.99, -0.9, 0, 0.9, 0.99)
 # define stationary models
-statparams = cbind(rep(alpha = 0.9, length(beta)), beta = c(-0.99, -0.9, 0, 0.9, 0.99))
+statparams = cbind(rep(0.9, length(beta)), beta = c(-0.99, -0.9, 0, 0.9, 0.99))
 # define explosive models
-expparams  = cbind(rep(alpha = 1, length(beta)), beta = c(-0.99, -0.9, 0, 0.9, 0.99))
+expparams  = cbind(rep(1, length(beta)), beta = c(-0.99, -0.9, 0, 0.9, 0.99))
 #define lag vector
 pvec         = as.matrix(3:11)
 # define significance level
@@ -38,8 +38,8 @@ generateprocess = function(alpha, beta, iSample){
 # adftest for varying p
 adftestvaryp = function(pvec, x){
   adftestonep = function(p){return(adf.test(x, alternative = c("stationary"), k = p)$p.value)}
-  pvalues = apply(pvec, 1, adftestonep)
-  rejection = as.numeric(pvalues < isiglevel)
+  pvalues     = apply(pvec, 1, adftestonep)
+  rejection   = as.numeric(pvalues < isiglevel)
   return(rejection)
 }
 
@@ -47,7 +47,7 @@ adftestvaryp = function(pvec, x){
 ADFSimtest = function(alpha, beta){
   rejection = matrix(rep(NaN, iSimulations, length(pvec)), nrow = iSimulations, ncol = length(pvec))
   for(iCounterSim in 1:iSimulations){
-    x = simprocess(alpha, beta, iSample)
+    x                        = generateprocess(alpha, beta, iSample)
     rejection[iCounterSim, ] = adftestvaryp(pvec, x)}
   rejectionprob = colMeans(rejection)
   return(rejectionprob)
@@ -62,5 +62,7 @@ matplot(teststat, type = "l", lwd = 3, ylab = "Rejection Probability", xlab = "L
 matplot(testexp, type = "l", lwd = 3, ylab = "Rejection Probability", xlab = "Lags", main = "Power of ADF")
 
 # create latex table
-colnames(teststat) = sapply(beta, as.character)
-rownames(testexp[c(1, 9), ]) = sapply(c(3,11), as.character)
+tablestationary = round(teststat[c(1, 9), ], 2)
+tableexplosive  = round(testexp[c(1, 9), ], 2)
+colnames(tablestationary) = sapply(beta, as.character)
+rownames(tablestationary) = sapply(c(3, 11), as.character)
